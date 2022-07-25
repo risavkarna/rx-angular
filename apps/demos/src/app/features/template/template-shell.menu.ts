b2de3ded4c0247507d555ee6782270e958d2b951
Julian Jandl <julian@jandl.wien>
2022-07-25 17:16:43 +0200

    feat(demos): implement rxChunk demo application

--- apps/demos/src/app/features/template/template-shell.menu.ts
+++ apps/demos/src/app/features/template/template-shell.menu.ts
@@ -1,3 +1,4 @@
+import { RX_CHUNK_MENU_ITEMS } from './chunk/rx-chunk.menu';
 import { MENU_ITEMS as RX_LET_MENU_ITEMS } from './rx-let/rx-let.menu';
 import { MENU_ITEMS as RX_IF_MENU_ITEMS } from './rx-if/rx-if.menu';
 import { MENU_ITEMS as RX_CONTEXT_MENU_ITEMS } from './rx-context/rx-context.menu';
@@ -7,7 +8,6 @@ import { MENU_ITEMS as STRATEGY_MENU_ITEMS } from './strategies/concurrent-strat
 import { MENU_ITEMS as PIPES_MENU_ITEMS } from './pipes/pipes.menu';
 
 export const TEMPLATE_MENU = [
-
   {
     label: 'Pipes',
     link: 'pipes',
@@ -34,12 +34,17 @@ export const TEMPLATE_MENU = [
     children: RX_FOR_MENU_ITEMS,
   },
   {
+    label: '*rxChunk',
+    link: 'rx-chunk',
+    children: RX_CHUNK_MENU_ITEMS,
+  },
+  {
     link: 'render-callback',
-    label: 'Render Callback'
+    label: 'Render Callback',
   },
   {
     label: 'Unpatch',
-    link: 'unpatch'
+    link: 'unpatch',
   },
   {
     label: 'Strategies',
@@ -50,5 +55,5 @@ export const TEMPLATE_MENU = [
     label: 'ViewPort Prio',
     link: 'view-port-prio',
     children: VIEWPORT_PRIO_MENU_ITEMS,
-  }
+  },
 ];
